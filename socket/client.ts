39da261e388ce96ee49785e7a6de865e9a3cc89b
gemlorenz10 <gemlorenz10@gmail.com>
2017-12-01 20:52:57 +0800

    make socket js 1 file

--- socket/client.ts
+++
@@ -1,6 +0,0 @@
-function connect( server ){
-    var socket = io( server );
-    socket.on('reload', ( data )=>{
-        location.reload();
-    });
-}
--- socket/default.ts
+++
@@ -1,4 +0,0 @@
-let socket = io('http://localhost:9000');
-socket.on('reload', ( data )=>{
-    location.reload();
-});
